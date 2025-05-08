7ffacc92e4f6dbdbda5450c0ab8510c6218b22f0
PAWAR GUNJAN SINGH <71166527+gunjansinghpawar@users.noreply.github.com>
2025-05-08 17:58:20 +0530

    Update setTokenCookies.js

--- utils/setTokenCookies.js
+++ utils/setTokenCookies.js
@@ -2,11 +2,13 @@ const setTokenCookies = (res, accessToken, refreshToken) => {
     res.cookie('jwtAccessToken', accessToken, {
         httpOnly: true,
         secure: true,
+        sameSite: 'None',
         expires: new Date(Date.now() + 1000 * 60 * 15), // 15 minutes
     });
     res.cookie('refreshToken', refreshToken, {
         httpOnly: true,
         secure: true,
+        sameSite: 'None',
         expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 7), // 7 days
     });
 };
